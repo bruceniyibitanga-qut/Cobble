7bdfe72eb78dc8292377b34b99e768425f204918
bruceniyibitanga <bruce.niyibitanga@connect.qut.edu.au>
2026-05-14 23:21:07 +1000

    Updated CORS to allow hosted frontend

--- api/CobbleAPI/CobbleAPI/Program.cs
+++ api/CobbleAPI/CobbleAPI/Program.cs
@@ -20,7 +20,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://localhost:5501",
-                "http://127.0.0.1:5501"
+                "http://127.0.0.1:5501",
+                "https://frontend-staging-cf49.up.railway.app"
               )
               .AllowAnyHeader()
               .AllowAnyMethod();
