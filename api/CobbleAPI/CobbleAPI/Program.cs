a08765d60ca1e6b5340b67a08951eaef2be9245d
bruceniyibitanga <bruce.niyibitanga@connect.qut.edu.au>
2026-05-14 23:50:45 +1000

    Assertained CORS issue. Reverting back

--- api/CobbleAPI/CobbleAPI/Program.cs
+++ api/CobbleAPI/CobbleAPI/Program.cs
@@ -14,7 +14,15 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevCors", policy =>
     {
-        policy.SetIsOriginAllowed(_ => true)  // Allow any origin temporarily
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "http://localhost:5500",
+                "http://127.0.0.1:5500",
+                "http://localhost:5501",
+                "http://127.0.0.1:5501",
+                "https://frontend-staging-cf49.up.railway.app"
+              )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
